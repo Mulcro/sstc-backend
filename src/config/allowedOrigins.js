e02605598acf44569fe8d25c06eea0219c0a577f
Mulero Alamou <Mulero.alamou@gmail.com>
2024-07-20 22:26:29 -0700

    minor modifications

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,5 +1,11 @@
 const allowedOrigins = [
+    //VS Code postman agent apparently has an undefined origin. Remember to remove this in production
+    undefined,
     'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:4000',
+    'http://localhost:4000',
+    'http://127.0.0.1:4173',
     "https://web.postman.co"
 ];
 
