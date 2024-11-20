3c4e8d5ba358b4221866b8ba09dca99b896f31c2
Mulero Alamou <Mulero.alamou@gmail.com>
2024-11-19 20:41:55 -0800

    added comment about env secret config

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectDb = async () => {
 
     try{
+        // Set prod and testing mongo uri in github repo env secrets
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
